522d97310390a97f1de877666df21c520336ff3f
Martin-J-Larre <martinjlarre@gmail.com>
2022-10-31 00:33:44 -0400

    #2 routes started

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// imports
+const routes = require('./routes');
+
+// routes
+app.use('/api/1.0', routes());
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+
+const { application } = require('express');
+const express = require('express');
+const router = express.Router();
+
+module.exports = function() {
+    router.get('/',(req, res) =>{
+        res.send('Index');
+    });
+    router.get('/nosotros', (req, res) => {
+        res.send('Nosotros');
+    });
+    return router;
+}
