0c3d95391630a500656974f34f786799bc893f3f
Martin-J-Larre <martinjlarre@gmail.com>
2022-11-04 00:56:30 -0400

    #7 route/controller/views creatProject section

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,17 +1,28 @@
-exports.createOne = (req, res) => {
-    res.send('Index');
-}
-exports.getOne = (req, res) => {
-    res.send('Get One');
-}
-exports.getAll = (req, res) => {
+// exports.createOne = (req, res) => {
+//     res.send('Index');
+// }
+// exports.getOne = (req, res) => {
+//     res.send('Get One');
+// }
+// exports.getAll = (req, res) => {
+//     res.render('index', {
+//         pageName: 'Projects'
+//     })
+// }
+// exports.updateOne = (req, res) => {
+//     res.send('Update one');
+// }
+// exports.deleteOne = (req, res) => {
+//     res.send('Delete One');
+// }
+// Projects-views
+exports.projectHome = (req, res) => {
     res.render('index', {
         pageName: 'Projects'
     })
 }
-exports.updateOne = (req, res) => {
-    res.send('Update one');
-}
-exports.deleteOne = (req, res) => {
-    res.send('Delete One');
+exports.newProject = (req, res) => {
+    res.render('newProject', {
+        pageName: 'New Project'
+    })
 }
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const projectsController = require('../controllers/projectsController');
 
 module.exports = function() {
-    router.post('/', projectsController.createOne);
-    router.get('/', projectsController.getAll);
-    router.get('/:id', projectsController.getOne);
-    router.put('/:id', projectsController.updateOne);
-    router.delete('/:id', projectsController.deleteOne);
+    router.get('/', projectsController.projectHome);
+    router.post('/new-project', projectsController.newProject);
+    // router.get('/:id', projectsController.getOne);
+    // router.put('/:id', projectsController.updateOne);
+    // router.delete('/:id', projectsController.deleteOne);
    
     return router;
 }
