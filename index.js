ecdb61f11f72c0d542d85aaeb45e219d71d01d9a
Martin-J-Larre <martinjlarre@gmail.com>
2022-11-09 01:11:12 -0500

    #14 Helpers added

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config()
-const port = 5000
+
+
+// imports
+const routes = require('./routes');
+const helpers = require('./helpers/helpers')
+
+const port = 5000;
+
 
 // DB
 const sequelize = require('./config/db');
@@ -12,24 +19,21 @@ require('./models/Projects');
 sequelize.sync()
     .then(()=> console.log("*** Database Conected ***"))
     .catch(err => console.log("=== Database not Connected ===", err));
-  
-
-
-// imports
-const routes = require('./routes');
-
-app.use(express.static('public'));
 
 // template engine
+app.use(express.static('public'));
 app.set('view engine', 'pug');  
 app.set('views', path.join(__dirname, './views'));
 
 //middlewares
 app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res, next) => {
+  res.locals.vardump = helpers.vardump;
+  next();
+});
 
 // routes
 app.use('/', routes());
-
 
 
 app.listen(port, () => {
